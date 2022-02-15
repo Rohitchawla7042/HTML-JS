54b9c2e43e9ddefd0279d63740bb076c10be0c26
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-02-15 19:31:07 +0530

    variables
    Just started with Js and gone through some topic such as variables where we print using console.log and also gone through the topic of operators

---
+++ variables.js
@@ -0,0 +1,32 @@
+console.log('Hello, please have a look');
+
+var a =78;
+var b ="String stored";
+console.log(b);
+c = 18584; 
+
+// operators in js 
+// unary opertor works on only one operand 
+
+c = -c ; 
+console.log(c);
+console.log('Unary operator ^^^'); 
+
+// Binary operaor works on two operands 
+
+d = 5654545*65466556; 
+console.log(d);
+console.log('Binary operator ^^');
+
+
+var num1 = 45; 
+var num2 = 35; 
+
+console.log("The value of num1 + num2 is : " + (num1 + num2));
+console.log("The value of num1 - num2 is : " + (num1 - num2));
+console.log("The value of num1 * num2 is : " + (num1 * num2));
+console.log("The value of num1 / num2 is : " + (num1 / num2));
+console.log("The value of num1++ : " + (num1++));
+console.log("The value of num1++ : " + (++num1));
+
+
