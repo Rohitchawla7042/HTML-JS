1349e3c095280c30d30d57887e2bb788f01689d1
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-02-15 21:22:33 +0530

    Strings
    This is the commit with some strings data and tomorrow i will do the string functions .

---
+++ strings.js
@@ -0,0 +1,12 @@
+console.log("Hello , please check");
+
+var string = "Hey!"; 
+var name = "hi"; 
+console.log(string);
+var temp = `${string} please have a look over this and it got successfully compiled which is called ${name}`;
+console.log(temp);
+var len = name.length; 
+console.log(`length of the string is ${len}`); 
+console.log("Hello world\n hey!!"); 
+
+document.getElementById('heck').innerHTML = '<h3>This is the new content</h3>'
