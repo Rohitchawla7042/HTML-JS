b9aa44c0bf5b9a53fe22774f8f6d4b607056a6f1
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-02-18 22:36:20 +0530

    Update Strings.js

--- strings.js
+++ strings.js
@@ -9,4 +9,43 @@ var len = name.length;
 console.log(`length of the string is ${len}`); 
 console.log("Hello world\n hey!!"); 
 
-document.getElementById('heck').innerHTML = '<h3>This is the new content</h3>'
+// String functions
+
+
+var str = "Rohit is fine";
+console.log(str);
+var position = str.indexOf('i');
+console.log(position);
+position = str.lastIndexOf('f');
+console.log(position);
+// substring of a string 
+var substr = str.slice(1,3);
+console.log(substr)
+// replacing a value 
+
+// console.log(str) as it replaced fine with nice
+var replace = str.replace('fine', 'nice');
+console.log(str);
+console.log(replace);
+
+// uppercase and lowercase 
+
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+
+// concat method 
+
+var newstring = str.concat(' and good');
+console.log(newstring); 
+
+
+// to remove whitespaces 
+
+var stringWith = "      Hello             bhxjsxhjjdkxqxdk       jnkjbhjkjb   ";
+console.log(stringWith)
+console.log(stringWith.trim());
+
+// extract char from string 
+
+var char1 = str.charAt(2);
+console.log(char1);
