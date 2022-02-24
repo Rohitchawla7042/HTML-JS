926f4759a75628587e3c1e23f4fca657f999e9a0
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-02-24 20:21:24 +0530

    Add files via upload
    Here we talk about arrays and objects

---
+++ arrays.js
@@ -0,0 +1,34 @@
+console.log('arrays and objects')
+
+let myvar = 34;
+
+// Objects which we are creating manually 
+
+let employee =
+{
+    name:'Rohit',
+    age : 20 , 
+    salary: 20000,
+    Male: 'true',
+}
+console.log(employee);
+
+// Arrays 
+
+let array1 =[1,2,3,"Rohit"];
+console.log(array1[3])
+console.log(array1)
+console.log(array1.length)
+
+// Sort an Array 
+
+let array2 =[45,48,25,27,15,'Rohit'];
+console.log(array2)
+sort1 = array2.sort();
+console.log(sort1)
+
+// how to insert any value in the aray in last 
+
+array2.push("This element is pushed at last")
+console.log(array2)
+
