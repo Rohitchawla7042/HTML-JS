f2994d7f1b5192e0e5ea44987385326ed4121fcc
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-04-05 16:29:53 +0530

    Interactive Site
    Here i have done changes such as when system ask the user to enter details such as prompt box , confirm box and alert box on the webpage

---
+++ interactive page.js
@@ -0,0 +1,21 @@
+console.log("checked")
+// Alert in browser js this will get display on your screen as a popup but it doesn't return anything .
+// alert("This is a message");
+
+// when we want to ask something from user and the page wants to give some message to the user 
+
+//  let name = prompt("What is your name ?");
+//  console.log(name);
+
+// //  confirm button 
+
+// let deletePost = confirm("Do you really want to delete the data ")
+// if(deletePost){console.log("Your post is deleted");}
+// else{console.log("Your post is not deleted");}
+// // console.log(deletePost);
+
+let number = prompt("Please enter your age");
+confirm("Is this is the right age");
+console.log(number);
+if(number>18){console.log("Allowed");}
+else{console.log("Not allowed");}
