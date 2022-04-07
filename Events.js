190ecbddd5c908fc976a1cf866e2b59a187dedf2
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-04-07 20:43:39 +0530

    Events in JS
    Here are some events in JS , there are many more events has to come

---
+++ Events.js
@@ -0,0 +1,24 @@
+console.log('Checking events and listeners');
+function togglehide(){
+   let btn = document.getElementById('btn');
+   let para = document.getElementById('para');
+   if(para.style.display!= 'none'){
+       para.style.display = 'none';
+    }
+   else{
+    para.style.display = 'block';
+   }
+}
+function togglehide1(){
+    {
+    //     let para1 = document.getElementById('para1');
+    //     let btn1 = document.getElementById('btn1');
+    // }
+    if(para1.style.display!= 'none'){
+        para1.style.display = 'none';
+    }
+    else{
+        para1.style.display = 'block';
+    }
+}
+
