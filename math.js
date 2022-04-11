29fc2057412a64276facc37925e9f5ba50b2b392
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-04-11 20:52:13 +0530

    Math Functions
    Here i had talk about various math objects such as pow , sqrt , math.ceil , floor , random nu , etc

---
+++ math.js
@@ -0,0 +1,42 @@
+console.log("Checking!!")
+let m = Math; 
+console.log(m);
+
+console.log("The value of Math.E is",Math.E )
+console.log("The value of Math.PI is",Math.PI )
+console.log("The value of Math.LN2 is",Math.LN2 )
+console.log("The value of Math.SQRT2 is",Math. SQRT2)
+
+//Printing the  Functions 
+
+let a = 55.7876879; 
+let b = 90.99;
+
+console.log("The value of a and b is ", a , b);
+console.log("The value of rounded a and rounded b is ", Math.round(a) , Math.round(b));
+
+console.log("3 raised to the power of 2 is " , Math.pow(3,2));
+console.log("Sqaare root of 36 is " , Math.sqrt(36));
+
+// ceil and floor value 
+
+console.log("The round up value of 5.96 is " , Math.ceil(5.96));
+console.log("The round down value of 5.66 is " , Math.floor(5,66));
+
+// Random Number 
+
+// a + b-a*Math.random ( for example 1 + 100 - 1 = 100)
+let a1 = 10; 
+let b1 = 50;
+let number = a1 + (b1 - a1)*(Math.random());
+console.log("The random number is " , number);
+
+
+E: 2.718281828459045
+LN2: 0.6931471805599453
+LN10: 2.302585092994046
+LOG2E: 1.4426950408889634
+LOG10E: 0.4342944819032518
+PI: 3.141592653589793
+SQRT1_2: 0.7071067811865476
+SQRT2: 1.4142135623730951
